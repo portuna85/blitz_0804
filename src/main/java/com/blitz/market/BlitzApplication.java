0514e3ec6fa8a9e15e5c6d0412c833ac41fbc8d3
portuna85 <portuna85@gmail.com>
2022-08-02 23:53:35 +0900

    update

--- src/main/java/com/blitz/market/BlitzApplication.java
+++ src/main/java/com/blitz/market/BlitzApplication.java
@@ -2,12 +2,20 @@ package com.blitz.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class BlitzApplication {
+@EnableJpaAuditing
+public class BlitzApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BlitzApplication.class, args);
     }
-
 }
