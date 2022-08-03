130e836e23bbd866b9b81f18ff29a23d0f61e6a2
portuna85 <portuna85@gmail.com>
2022-08-04 02:17:32 +0900

    Update

--- src/main/java/com/blitz/market/BlitzApplication.java
+++ src/main/java/com/blitz/market/BlitzApplication.java
@@ -1,13 +1,9 @@
 package com.blitz.market;
 
-import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
