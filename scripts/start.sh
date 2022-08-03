9774c19a959a6e80ccbf8df403f1db2dcdf85148
portuna85 <portuna85@gmail.com>
2022-08-03 17:34:50 +0900

    Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,13 +9,13 @@ PROJECT_NAME=blitz
 
 echo "> Build 파일 복사"
 
-echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.war $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.war | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
