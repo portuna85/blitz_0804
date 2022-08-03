b024f4a02034350c3b66d24b0d12555233bda8db
portuna85 <portuna85@gmail.com>
2022-08-03 20:44:25 +0900

    Update

--- src/main/java/com/blitz/market/BlitzApplication.java
+++ src/main/java/com/blitz/market/BlitzApplication.java
@@ -1,9 +1,13 @@
 package com.blitz.market;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -17,6 +21,20 @@ public class BlitzApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlitzApplication.class, args);
+    }
+
+    @Bean
+    public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader mustacheTemplateLoader,
+                                              Environment environment) {
+
+        MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+        collector.setEnvironment(environment);
+
+        // default value
+        Mustache.Compiler compiler = Mustache.compiler().defaultValue("")
+                .withLoader(mustacheTemplateLoader)
+                .withCollector(collector);
+        return compiler;
     }
 }
 
